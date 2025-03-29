2ba45668d338040b435e2659862d040d931b981f
Omeje Chidozie Stanley <omeje@systemspecs.com.ng>
2025-03-29 12:38:02 +0100

    first commit

---
+++ src/test/java/com/stan/appsecurity/AppSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stan.appsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
